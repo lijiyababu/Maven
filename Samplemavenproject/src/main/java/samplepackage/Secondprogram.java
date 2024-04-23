651d912e7fd530e82a4bcbbffba32f55c1b709e8
Lijiya <Lijiya@DESKTOP-B1S4JGN>
2024-04-23 23:52:38 +0530

    commit

--- Samplemavenproject/src/main/java/samplepackage/Secondprogram.java
+++ Samplemavenproject/src/main/java/samplepackage/Secondprogram.java
@@ -4,9 +4,9 @@ public class Secondprogram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String name = "nathan";
+		String name = "Nathan ";
 		StringBuilder obj = new StringBuilder(name);
-		obj.append(" moncy pappachan");
+		obj.append(" Moncy pappachan");
 		System.out.println(obj);
 	}
 
